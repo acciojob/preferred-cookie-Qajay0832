feac1b20d26baf7a1adfc94df586b2d20484bfd0
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-07-07 16:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+saveBtn=document.getElementById("submit");
+fontcolor=document.getElementById("fontcolor");
+fontsize=document.getElementById("fontsize");
+saveBtn.addEventListener("click",save);
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    if(document.cookie){
+     let ele =document.cookie.split(';')
+     ele.forEach(e=>{
+        value=e.trim().split('=');
+        if(value[0]=='fontcolor'){
+            fontcolor.value=value[1];
+        }
+        else if(value[0]=='fontsize'){
+            fontsize.value=value[1];
+        }
+     })  
+    }
+    
+
+})
+function save(e){
+    e.preventDefault();
+    document.cookie=`fontcolor=${fontcolor.value};`
+    document.cookie=`fontsize=${fontsize.value};`
+
+}
